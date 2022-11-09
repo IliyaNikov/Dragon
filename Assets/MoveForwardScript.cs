8c6df0ab07de5b804c29a49d18571ebcb525142a
IliyaNikov <74715206+IliyaNikov@users.noreply.github.com>
2022-11-09 15:26:06 +0100

    Dragon rotates only around itself

---
+++ Assets/MoveForwardScript.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:42a1d00ba566125da2df5cc24d4e752468c063c0c6f110b2027fc464f7decca5
+size 363
---
+++ Assets/RotationMovementScript.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0ea85123cb13ee79abb35307ed67e9feb89f2a531eb61d4ae0bd55604ea00c63
+size 467
